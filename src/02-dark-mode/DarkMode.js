b6e50ad435ee20ff51d7b0a613a5b88a5544a1d7
arturo <ahpareja@gmail.com>
2023-01-02 19:08:24 -0500

    fff

--- src/02-dark-mode/DarkMode.js
+++ src/02-dark-mode/DarkMode.js
@@ -1,8 +1,20 @@
 export default function DarkMode () {
+  function toggleDark(){
+    let element = document.querySelector('.page');
+    element.classList.add("dark-mode");
+  }
+
+  function toggleLight(){
+    let element = document.querySelector('.page');
+    element.classList.remove("dark-mode");
+  }
+
   return (
     <div className='page'>
-      <button className='dark-mode-button'>Dark Mode</button>
-      <button className='light-mode-button'>Light Mode</button>
+      <button className='dark-mode-button' onClick={toggleDark}>Dark Mode</button>
+      <button className='light-mode-button' onClick={toggleLight}>Light Mode</button>
     </div>
   )
 }
+
+
